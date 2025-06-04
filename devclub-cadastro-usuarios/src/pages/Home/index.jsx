621114f596704db284631b85c3d5e2d161de9e9f
Bruna Aquino <brunahasting99@gmail.com>
2025-06-04 14:36:18 -0300

    fiz a instalaçao da tailwind

--- devclub-cadastro-usuarios/src/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,8 +1,37 @@
 import "./style.css";
+import { ArchiveX } from "lucide-react";
 
 function Home() {
+  // RASCUNHO PARA TESTES
+  const users = [
+    {
+      id: "2345",
+      name: "Rodolfo",
+      age: 33,
+      email: "rod@email.com",
+    },
+    {
+      id: "23456",
+      name: "Aline",
+      age: 25,
+      email: "aline@email.com",
+    },
+    {
+      id: "234567",
+      name: "Bruna",
+      age: 26,
+      email: "bru@email.com",
+    },
+    {
+      id: "2345678",
+      name: "Paulo",
+      age: 30,
+      email: "paulo@email.com",
+    },
+  ];
+
   return (
-    <div className="container">
+    <div className="">
       <form action="">
         <h1>Cadastro de Usuários</h1>
         <input name="nome" type="text" />
@@ -10,14 +39,23 @@ function Home() {
         <input name="email" type="email" />
         <button type="button">Cadastrar</button>
       </form>
-      <div>
-        <div>
-          <p>Nome:</p>
-          <p>Idade:</p>
-          <p>Email:</p>
-        </div>
-        <button><ArchiveX /></button>
-      </div>
+
+      {users.map(
+        (
+          user //O React precisa de uma key, uma chave que nao se repita e no nosso caso e o id.
+        ) => (
+          <div key={user.id}>
+            <div>
+              <p>Nome: {user.name}</p>
+              <p>Idade: {user.age}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <button>
+              <ArchiveX />
+            </button>
+          </div>
+        )
+      )}
     </div>
   );
 }
