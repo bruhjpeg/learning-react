00351b2b2504d0ce2f9cd139094f1607baa5e6c2
Bruna Assis <105996694+bruhjpeg@users.noreply.github.com>
2025-06-07 17:10:06 -0300

    Conectando o front com o back
    Get conectado com o front

--- devclub-cadastro-usuarios/src/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,34 +1,20 @@
 import "./style.css";
 import { ArchiveX } from "lucide-react";
+import api from "../../services/api";
+import { useEffect, useState } from "react";
 
 function Home() {
-  // RASCUNHO PARA TESTES
-  const users = [
-    {
-      id: "2345",
-      name: "Rodolfo",
-      age: 33,
-      email: "rod@email.com",
-    },
-    {
-      id: "23456",
-      name: "Aline",
-      age: 25,
-      email: "aline@email.com",
-    },
-    {
-      id: "234567",
-      name: "Bruna",
-      age: 26,
-      email: "bru@email.com",
-    },
-    {
-      id: "2345678",
-      name: "Paulo",
-      age: 30,
-      email: "paulo@email.com",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    const usersFromSpi = await api.get("/usuarios");
+
+    setUsers(usersFromSpi.data);
+    console.log(users);
+  }
+  useEffect(() => {
+    getUsers();
+  });
 
   return (
     <div className="container">
