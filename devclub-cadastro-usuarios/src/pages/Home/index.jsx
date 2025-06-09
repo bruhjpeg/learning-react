3ebac739228000d0a17ba1cd03d1ba555e122da2
Bruna Assis <105996694+bruhjpeg@users.noreply.github.com>
2025-06-09 13:30:45 -0300

    Update index.jsx

--- devclub-cadastro-usuarios/src/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,29 +1,54 @@
 import "./style.css";
 import { ArchiveX } from "lucide-react";
 import api from "../../services/api";
-import { useEffect, useState,  } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function Home() {
   const [users, setUsers] = useState([]);
 
-  async function getUsers() {
-    const usersFromSpi = await api.get("/usuarios");
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
 
-    setUsers(usersFromSpi.data);
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: Number(inputAge.current.value),
+      email: inputEmail.current.value,
+    });
+    getUsers();
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`);
+    getUsers();
   }
 
   useEffect(() => {
     getUsers();
-  });
+  }, []);
 
   return (
     <div className="container">
       <form action="">
         <h1>Cadastro de Usuários</h1>
-        <input placeholder="Nome" name="nome" type="text" />
-        <input placeholder="Idade" name="idade" type="number" />
-        <input placeholder="E-mail" name="email" type="email" />
-        <button type="button">Cadastrar</button>
+        <input placeholder="Nome" name="nome" type="text" ref={inputName} />
+        <input placeholder="Idade" name="idade" type="number" ref={inputAge} />
+        <input
+          placeholder="E-mail"
+          name="email"
+          type="email"
+          ref={inputEmail}
+        />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
       </form>
 
       {users.map(
@@ -42,7 +67,7 @@ function Home() {
                 Email: <span> {user.email}</span>
               </p>
             </div>
-            <button>
+            <button onClick={() => deleteUsers(user.id)}>
               <ArchiveX />
             </button>
           </div>
